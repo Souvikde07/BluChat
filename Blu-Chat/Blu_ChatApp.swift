3397830f78fb3f0679afe120fdb0762c944d16d0
Souvik De <souvik.de5@gmail.com>
2026-07-28 01:12:34 +0900

    Initial Commit

---
+++ Blu-Chat/Blu_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blu_ChatApp.swift
+//  Blu-Chat
+//
+//  Created by Souvik De on 28/07/26.
+//
+
+import SwiftUI
+
+@main
+struct Blu_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blu-Chat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blu-Chat
+//
+//  Created by Souvik De on 28/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
